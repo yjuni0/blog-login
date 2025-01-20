799ff9a34bda0bf67b68753a3dac91640b516bd1
yejun <alstn9654@naver.com>
2025-01-20 15:08:38 +0900

    리프레시 토큰

--- src/main/java/com/project/blog/repository/RefreshTokenRepository.java
+++ src/main/java/com/project/blog/repository/RefreshTokenRepository.java
@@ -2,21 +2,10 @@ package com.project.blog.repository;
 
 import com.project.blog.entity.RefreshToken;
 import org.springframework.data.jpa.repository.JpaRepository;
-import com.project.blog.entity.User;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
 
 @Repository
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
-    // 사용자로 리프레시 토큰 찾기
-    Optional<RefreshToken> findByUser(User user);
 
-    // 토큰 문자열로 리프레시 토큰 찾기
-    Optional<RefreshToken> findByToken(String token);
-    // 사용자 ID로 리프레시 토큰 삭제
-    void deleteByUser_Id(Long userId);
-
-    // 특정 토큰 값으로 삭제
-    void deleteByToken(String token);
 }
